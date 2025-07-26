9aba3112daf1290456897b5160c4f7d51ae956cc
Ryan Baumgardner <ryan.baumgardner@protonmail.com>
2025-07-26 10:20:26 -0400

    feat: add initial lsp setup

--- init.lua
+++ init.lua
@@ -4,69 +4,69 @@
 vim.cmd.colorscheme("default")
 
 -- general
-vim.opt.number = true					-- make line numbers default
-vim.opt.relativenumber = true				-- relative line numbers
-vim.opt.cursorline = true				-- highlight current li
-vim.opt.wrap = false					-- don't wrap lines
-vim.opt.scrolloff = 10					-- keep 10 lines above and below cursor
+vim.opt.number = true         -- make line numbers default
+vim.opt.relativenumber = true -- relative line numbers
+vim.opt.cursorline = true     -- highlight current li
+vim.opt.wrap = false          -- don't wrap lines
+vim.opt.scrolloff = 10        -- keep 10 lines above and below cursor
 
 -- indentation
-vim.opt.smartindent = true				-- smart auto-indenting
-vim.opt.autoindent = true 				-- copy indent from current line
+vim.opt.smartindent = true -- smart auto-indenting
+vim.opt.autoindent = true  -- copy indent from current line
 
 -- search
-vim.opt.ignorecase = true				-- case insensitive search
-vim.opt.smartcase = true				-- case insensetive if uppercase in search
-vim.opt.hlsearch = false				-- don't highlight search results
-vim.opt.incsearch = true				-- show matches as you type
-vim.opt.inccommand = 'split'				-- preview substitutions as you type
+vim.opt.ignorecase = true    -- case insensitive search
+vim.opt.smartcase = true     -- case insensetive if uppercase in search
+vim.opt.hlsearch = false     -- don't highlight search results
+vim.opt.incsearch = true     -- show matches as you type
+vim.opt.inccommand = 'split' -- preview substitutions as you type
 
 -- visual
-vim.opt.termguicolors = true				-- enable 24-bit colors
-vim.opt.signcolumn = "auto" 				-- always show sign column
-vim.opt.colorcolumn = '80'				-- show color column at 80 characters
-vim.opt.showmatch = true				-- highlight matching brackets
-vim.opt.cmdheight = 1					-- command line height
-vim.opt.completeopt = "menuone,noinsert,noselect" 	-- completion options
-vim.opt.pumheight = 10 					-- popup menu height
-vim.opt.pumblend = 10 					-- popup menu transparency
-vim.opt.winblend = 0					-- floating window transparency
-vim.opt.conceallevel = 0				-- don't hide markup
-vim.opt.concealcursor = ""				-- don't hide cursor line markup
-vim.opt.lazyredraw = true				-- don't redraw during macros
-vim.opt.synmaxcol = 300					-- don't syntax highlihgt after 300
+vim.opt.termguicolors = true                      -- enable 24-bit colors
+vim.opt.signcolumn = "yes"                        -- always show sign column
+vim.opt.colorcolumn = '80'                        -- show color column at 80 characters
+vim.opt.showmatch = true                          -- highlight matching brackets
+vim.opt.cmdheight = 1                             -- command line height
+vim.opt.completeopt = "menuone,noinsert,noselect" -- completion options
+vim.opt.pumheight = 10                            -- popup menu height
+vim.opt.pumblend = 10                             -- popup menu transparency
+vim.opt.winblend = 0                              -- floating window transparency
+vim.opt.conceallevel = 0                          -- don't hide markup
+vim.opt.concealcursor = ""                        -- don't hide cursor line markup
+vim.opt.lazyredraw = true                         -- don't redraw during macros
+vim.opt.synmaxcol = 300                           -- don't syntax highlihgt after 300
 
 -- files
-vim.opt.backup = false					-- don't create backup files
-vim.opt.writebackup = false				-- don't create backup before writing
-vim.opt.swapfile = false				-- don't create swap files
-vim.opt.undofile = true					-- persistent undo
-vim.opt.undodir = vim.fn.expand("~/.vim/undodir")	-- undo directory
-vim.opt.autoread = true					-- auto reload files changed outside vim
-vim.opt.autowrite = false				-- don't auto save
-vim.opt.updatetime = 300				-- write to swapfile after 300ms
-vim.opt.timeoutlen = 500				-- time in ms to wait for sequence to complete
-vim.opt.ttimeoutlen = 0					-- time in ms to for key code timeot to complete
+vim.opt.backup = false                            -- don't create backup files
+vim.opt.writebackup = false                       -- don't create backup before writing
+vim.opt.swapfile = false                          -- don't create swap files
+vim.opt.undofile = true                           -- persistent undo
+vim.opt.undodir = vim.fn.expand("~/.vim/undodir") -- undo directory
+vim.opt.autoread = true                           -- auto reload files changed outside vim
+vim.opt.autowrite = false                         -- don't auto save
+vim.opt.updatetime = 300                          -- write to swapfile after 300ms
+vim.opt.timeoutlen = 500                          -- time in ms to wait for sequence to complete
+vim.opt.ttimeoutlen = 0                           -- time in ms to for key code timeot to complete
 
 -- editing
-vim.opt.hidden = true					-- allow hidden buffers
-vim.opt.errorbells = false				-- no error bells
-vim.opt.backspace = "indent,eol,start"			-- better backspace behavior
-vim.opt.autochdir = false				-- don't auto change directory
-vim.opt.iskeyword:append("-")				-- treat dash as part of word
-vim.opt.path:append("**")				-- include subdirectories in search
-vim.opt.selection = "exclusive"				-- selection behavior
-vim.opt.mouse = "a"					-- enable mouse support
-vim.opt.modifiable = true				-- allow buffer modification
-vim.opt.confirm = true					-- use dialog to confirm unsaved changes
+vim.opt.hidden = true                  -- allow hidden buffers
+vim.opt.errorbells = false             -- no error bells
+vim.opt.backspace = "indent,eol,start" -- better backspace behavior
+vim.opt.autochdir = false              -- don't auto change directory
+vim.opt.iskeyword:append("-")          -- treat dash as part of word
+vim.opt.path:append("**")              -- include subdirectories in search
+vim.opt.selection = "exclusive"        -- selection behavior
+vim.opt.mouse = "a"                    -- enable mouse support
+vim.opt.modifiable = true              -- allow buffer modification
+vim.opt.confirm = true                 -- use dialog to confirm unsaved changes
 -- schedule the setting after 'UiEnter' because it can increase startup-time
 vim.schedule(function()
-  vim.opt.clipboard = 'unnamedplus'			-- use system clipboard
+	vim.opt.clipboard = 'unnamedplus' -- use system clipboard
 end)
 
 -- splits
-vim.opt.splitright = true				-- vertical splits go to the right
-vim.opt.splitbelow = true				-- horizontal splits go below
+vim.opt.splitright = true -- vertical splits go to the right
+vim.opt.splitbelow = true -- horizontal splits go below
 
 -- keymaps
 
@@ -96,18 +96,49 @@ vim.keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'Keep it centered when moving u
 vim.keymap.set('n', '<leader>e', '<cmd>Explore<CR>', { desc = "Open file explorer" })
 
 -- fuzzy finder
-vim.keymap.set('n', '<leader>sf', '<cmd>FzfLua files<CR>', { desc = '[S]earch [F]iles'})
-vim.keymap.set('n', '<leader>sw', '<cmd>FzfLua live_grep<CR>', { desc = '[S]earch [W]ord'})
-vim.keymap.set('n', '<leader>sr', '<cmd>FzfLua resume<CR>', { desc = '[S]earch [R]esume'})
+vim.keymap.set('n', '<leader>sf', '<cmd>FzfLua files<CR>', { desc = '[S]earch [F]iles' })
+vim.keymap.set('n', '<leader>sw', '<cmd>FzfLua live_grep<CR>', { desc = '[S]earch [W]ord' })
+vim.keymap.set('n', '<leader>sr', '<cmd>FzfLua resume<CR>', { desc = '[S]earch [R]esume' })
 
 -- autocommands
-
 vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.hl.on_yank()
-  end,
+	desc = 'Highlight when yanking (copying) text',
+	group = vim.api.nvim_create_augroup('HighlightYankGroup', { clear = true }),
+	callback = function()
+		vim.hl.on_yank()
+	end,
+})
+
+vim.api.nvim_create_autocmd('LspAttach', {
+	group = vim.api.nvim_create_augroup('LspAttachGroup', {}),
+	callback = function(args)
+		local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+		if client:supports_method('textDocument/implementation') then
+			-- Create a keymap for vim.lsp.buf.implementation ...
+		end
+
+		-- Enable auto-completion. Note: Use CTRL-Y to select an item. |complete_CTRL-Y|
+		if client:supports_method('textDocument/completion') then
+			-- Optional: trigger autocompletion on EVERY keypress. May be slow!
+			-- local chars = {}; for i = 32, 126 do table.insert(chars, string.char(i)) end
+			-- client.server_capabilities.completionProvider.triggerCharacters = chars
+
+			vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
+		end
+
+		-- Auto-format ("lint") on save.
+		-- Usually not needed if server supports "textDocument/willSaveWaitUntil".
+		if not client:supports_method('textDocument/willSaveWaitUntil')
+		    and client:supports_method('textDocument/formatting') then
+			vim.api.nvim_create_autocmd('BufWritePre', {
+				group = vim.api.nvim_create_augroup('LspFormattingGroup', { clear = false }),
+				buffer = args.buf,
+				callback = function()
+					vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 })
+				end,
+			})
+		end
+	end,
 })
 
 -- plugin initialization
@@ -115,8 +146,8 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 require("config.lazy")
 require("lazy").setup({
 	spec = {
-	    -- import your plugins
-	    { import = "plugins" },
+		-- import your plugins
+		{ import = "plugins" },
 	},
 	-- Configure any other settings here. See the documentation for more details.
 	-- colorscheme that will be used when installing plugins.
---
+++ lua/plugins/nvim-lspconfig.lua
@@ -0,0 +1,49 @@
+local config = function()
+	-- lsp configuration
+	-- luals
+	vim.lsp.config['luals'] = {
+		-- Command and arguments to start the server.
+		cmd = { 'lua-language-server' },
+		-- Filetypes to automatically attach to.
+		filetypes = { 'lua' },
+		-- Sets the "root directory" to the parent directory of the file in the
+		-- current buffer that contains either a ".luarc.json" or a
+		-- ".luarc.jsonc" file. Files that share a root directory will reuse
+		-- the connection to the same LSP server.
+		-- Nested lists indicate equal priority, see |vim.lsp.Config|.
+		root_markers = { { '.luarc.json', '.luarc.jsonc' }, '.git' },
+		-- Specific settings to send to the server. The schema for this is
+		-- defined by the server. For example the schema for lua-language-server
+		-- can be found here https://raw.githubusercontent.com/LuaLS/vscode-lua/master/setting/schema.json
+		settings = {
+			Lua = {
+				runtime = {
+					version = 'LuaJIT',
+				},
+				diagnostics = {
+					globals = { "vim" }
+				},
+				format = {
+					enable = true,
+					defaultConfig = {
+						indent_style = "space",
+						indent_size = "2",
+					}
+				}
+			}
+		}
+	}
+
+	-- enable lsp
+	-- lua
+	vim.lsp.enable("luals")
+
+	-- typescript
+	vim.lsp.enable("ts_ls");
+end
+
+return {
+	"neovim/nvim-lspconfig",
+	config = config,
+	lazy = false,
+}
