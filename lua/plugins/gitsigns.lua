21cfa5b1aed5516e6ab4bfa0408a4425f22c0024
Ryan Baumgardner <ryan.baumgardner@protonmail.com>
2025-07-26 21:32:33 -0400

    feat(plugin): add gitsigns plugin

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,3 +1,17 @@
 return {
 	"lewis6991/gitsigns.nvim",
+	config = function()
+		local gitsigns = require("gitsigns")
+		gitsigns.setup({
+			on_attach = function()
+				vim.keymap.set("x", "<leader>s", function()
+					gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+				end)
+
+				vim.keymap.set("x", "<leader>u", function()
+					gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+				end)
+			end,
+		})
+	end,
 }
