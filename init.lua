592b322abf242e6aa64ea35fc03e9817ef1aa7cb
Ryan Baumgardner <ryan.baumgardner@protonmail.com>
2025-07-26 21:18:50 -0400

    refactor: replace cyberdream theme with tokyonight

--- init.lua
+++ init.lua
@@ -152,7 +152,7 @@ require("lazy").setup({
 	},
 	-- Configure any other settings here. See the documentation for more details.
 	-- colorscheme that will be used when installing plugins.
-	install = { colorscheme = { "cyberdream" } },
+	install = { colorscheme = { "tokyonight" } },
 	-- automatically check for plugin updates
 	checker = { enabled = true },
 })
--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,8 +1,16 @@
 return {
-	"scottmckendry/cyberdream.nvim",
-	lazy = false,
-	priority = 1000,
-	config = function()
-		vim.cmd.colorscheme("cyberdream")
-	end,
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.cmd([[ colorscheme tokyonight]])
+		end,
+	},
+	{
+		"scottmckendry/cyberdream.nvim",
+		enabled = false,
+		lazy = false,
+		priority = 1000,
+	},
 }
